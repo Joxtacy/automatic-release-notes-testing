8e36d930eac27502054f22f3dbf1dc5a167e8c04
Joxtacy <10127673+Joxtacy@users.noreply.github.com>
2024-07-11 12:44:19 +0200

    ci: no scope

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,1 +1,15 @@
-export default { extends: ["@commitlint/config-conventional"] };
+export default {
+	extends: ["@commitlint/config-conventional"],
+	rules: {
+		"scope-enum": [
+			2,
+			"always",
+			[
+				"frontend",
+				"backend",
+				"security",
+			],
+		],
+		"scope-empty": [1, "never"],
+	},
+};
