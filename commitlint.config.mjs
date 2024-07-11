9ad8f12fadefc7ba756f33a96e447ebdfa3940cd
Joxtacy <10127673+Joxtacy@users.noreply.github.com>
2024-07-11 17:38:51 +0200

    feat: add jira rule
    Adds a rule that checks for a Jira issue identifier in the footer.
    I.e.: something like "JIRA: ID-123"

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,5 +1,10 @@
 export default {
 	extends: ["@commitlint/config-conventional"],
+	parserPreset: {
+		parserOpts: {
+			noteKeywords: ["jira"],
+		},
+	},
 	rules: {
 		"scope-enum": [
 			2,
@@ -11,5 +16,26 @@ export default {
 			],
 		],
 		"scope-empty": [1, "never"],
+		"footer-leading-blank": [2, "always"],
+		"body-leading-blank": [2, "always"],
+		"tvx-jira-rule": [1, "always"],
 	},
+	plugins: [
+		{
+			rules: {
+				"tvx-jira-rule": (obj) => {
+					const { footer } = obj; // footer/noteKeywords are not case sensitive
+					const jiraRegex = new RegExp(`[A-Z]+-\\d+`);
+					if (!footer) {
+						return [false, "footer is missing"];
+					}
+
+					return [
+						jiraRegex.test(footer),
+						"footer should contain JIRA reference like ID-123",
+					];
+				},
+			},
+		},
+	],
 };
